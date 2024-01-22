45626a0c90bda7599610b278932666a2a63df29c
Abdurahman AYDIN <hariysu@gmail.com>
2024-01-22 16:11:18 +0300

    Initial Commit

---
+++ H4X0R-SwiftUI-Section17/H4X0R_SwiftUI_Section17App.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_SwiftUI_Section17App.swift
+//  H4X0R-SwiftUI-Section17
+//
+//  Created by Abdurahman on 22.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_SwiftUI_Section17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
